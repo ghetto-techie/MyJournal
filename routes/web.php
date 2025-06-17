1d145a1dded39e3dbbe9da915d46cf6886ec208b
GhettoTechie <ngonyoku.002@gmail.com>
2025-06-17 21:14:10 +0300

    Enabled email verification using MustVerifyEmail and middleware

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::get('/profile', function () {
+        return view('profile');
+    })->name('profile');
+});
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
 
 require __DIR__.'/auth.php';
